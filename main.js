824d3e8c46c1fab1ccdf2f75d11a6ec7e4b223a2
giovanalaureanti <giovana.laureanti.schultz@escola.pr.gov.br>
2025-10-29 07:57:33 -0300

    uhybvbu

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const avanca = document.querySelectorAll('.btn-proximo')
 console.log(avanca)
+
+avanca.forEach{buttin =>{
+   button.addEventListener('click', funcion(){
+       const atual =document.querySelector('.ativo')
+       const proximoPasso = 'passo-' + getAttribute('data-proximo')
+    
+       atual.classList.remove('ativo')
+       document.getElementById(proximoPasso).classList.add('ativo')
+    }) 
+})
