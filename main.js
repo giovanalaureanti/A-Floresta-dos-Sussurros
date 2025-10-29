2a747a336cee88af8026229f5679d2421815dcea
giovanalaureanti <giovana.laureanti.schultz@escola.pr.gov.br>
2025-10-29 11:47:15 -0300

    iubiub

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log(avanca)
 avanca.forEach{buttin =>{
    button.addEventListener('click', funcion(){
        const atual =document.querySelector('.ativo')
-       const proximoPasso = 'passo-' + getAttribute('data-proximo')
+       const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
     
        atual.classList.remove('ativo')
        document.getElementById(proximoPasso).classList.add('ativo')
